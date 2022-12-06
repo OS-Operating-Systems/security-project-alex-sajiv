54e77dad167e031f010ad43df462c4fdda6e08f4
SAJIV GNANASEKARAN <jsgnanasekaran@loyola.edu>
2022-12-06 00:42:24 -0500

    starting work on struct for bluetooth device

--- encryption.c
+++ encryption.c
@@ -2,10 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 #define MIN_FREQ 2400000000 //min frequency (hz) for bluetooth
 #define MAX_FREQ 2483500000 //max frequency (hz) for bluetooth
-#define KEY 3 //chosen key for frequency
+#define KEY 10082001 //chosen key for encrypt/decrypt
+#define KEY2 802001
+#define NUM_CHANNELS 37
+
+//a bluetooth device has a decrypted message and a channel map to transmit to
+struct BluetoothDevice {
+    char* decrypted_message;
+    int channel_map[NUM_CHANNELS]; //either 1 for used channel, or 0 for free channel
+};
+
+//a transmit package has an encrypted message and a frequency to transmit on
+struct TransmitPackage {
+    char* encrypted_message;
+    long frequency;
+};
+
+
+//method to initialize bluetooth device with message and channel map
+struct BluetoothDevice create_device(char in_message[], int channel_map_seed) {
+
+    struct BluetoothDevice device;
+
+    device.decrypted_message = in_message;
+
+    for(int i = 0; i < NUM_CHANNELS; i++) {
+        srand(channel_map_seed);   
+        device.channel_map[i] = rand() % (1 - 0 + 1) + 1; //1 for used, 0 for not used
+    }
+
+    return device;
+}
 
 
 long generate_freq() {
@@ -14,11 +43,16 @@ long generate_freq() {
     return freq;
 }
 
+void frequency_hop(long original_freq) {
+    //purpose: 
+
+}
+
 char* encrypt(char message[]) {
 
     int i;
     for(i = 0; i < strlen(message); i++) {
-        message[i] = message[i] + KEY;
+        message[i] = message[i] + (KEY/KEY2);
     }
 
     return message;
@@ -35,6 +69,11 @@ char* decrypt(char message[]) {
 
 int main() {
 
+    //initialize bluetooth devices
+    struct BluetoothDevice device1 = create_device("hi I am device 1");
+
+    /**/
+
     long freq = generate_freq();
     printf("%ld\n", freq);
 
@@ -42,8 +81,9 @@ int main() {
     char* encrypted_message = encrypt(message);
     printf("%s\n", encrypted_message);
 
-    char* decryped_message = decrypt(encrypted_message);
-    printf("%s\n", decryped_message);
+    char* decrypted_message = decrypt(encrypted_message);
+    printf("%s\n", decrypted_message);
 
+    */
     return 0;
 }
