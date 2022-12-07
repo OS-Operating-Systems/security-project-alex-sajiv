2ea5809c6091f70aeca0b41066fbe9fc589c3d50
Alex Kristian <ajkristian@loyola.edu>
2022-12-07 01:30:22 -0500

    AES source

--- aes.c
+++ aes.c
@@ -1,3 +1,5 @@
+//Source: https://github.com/kokke/tiny-AES-C
+
 /*
 This is an implementation of the AES algorithm, specifically ECB, CTR and CBC mode.
 Block size can be chosen in aes.h - available choices are AES128, AES192, AES256.
--- aes.h
+++ aes.h
@@ -1,3 +1,5 @@
+//Source: https://github.com/kokke/tiny-AES-C
+
 #ifndef _AES_H_
 #define _AES_H_
 
